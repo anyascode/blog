430d68ac57d380f004d916b1c707e501b0c3b322
Anya Pham <kieuanh997@gmail.com>
2025-05-26 20:29:28 +0300

    обработка ошибки

--- app/routes/articles.slug.tsx
+++ app/routes/articles.slug.tsx
@@ -24,7 +24,7 @@ export async function loader({ params }: Route.LoaderArgs) {
 }
 
 export default function ArticleSlug({ loaderData }: Route.ComponentProps) {
-  const { data, isLoading, isError } = useGetArticleQuery(loaderData?.postSlug);
+  const { data, isLoading, error } = useGetArticleQuery(loaderData?.postSlug);
   const userInfo = useSelector((state: RootState) => state.auth.userInfo);
   const [deleteArticle] = useDeleteArticleMutation();
   const [likeArticle] = useLikeArticleMutation();
@@ -40,24 +40,10 @@ export default function ArticleSlug({ loaderData }: Route.ComponentProps) {
       console.error("Like error:", err);
     }
   };
-  if (isError) {
-    return (
-      <div className="text-red-600 flex flex-row justify-center items-center pt-[251px]">
-        <ExclamationCircleIcon className="size-5" />{" "}
-        <p className="text-xl">{isError}</p>
-      </div>
-    );
-  }
+
   const navigate = useNavigate();
 
-  if (isError) {
-    return (
-      <div className="text-red-600 flex flex-row justify-center items-center pt-[251px]">
-        <ExclamationCircleIcon className="size-5" />{" "}
-        <p className="text-xl">{isError}</p>
-      </div>
-    );
-  }
+  console.log(error);
 
   const confirm: PopconfirmProps["onConfirm"] = async () => {
     try {
@@ -79,112 +65,121 @@ export default function ArticleSlug({ loaderData }: Route.ComponentProps) {
       ) : (
         <article className="py-[26px] px-[251px]">
           <div className="bg-white p-[16px] shadow-md flex flex-col gap-[25px]">
-            <div className="flex flex-row justify-between items-center overflow-hidden">
-              <div className="flex flex-col gap-[4px]">
+            {error ? (
+              <div className="text-red-600 flex flex-row justify-center items-center gap-3">
+                <ExclamationCircleIcon className="size-5" />{" "}
+                <p className="text-xl">Page not found</p>
+              </div>
+            ) : (
+              <>
                 {" "}
-                <h1
-                  className="flex flex-row text-sky-500 text-xl items-center gap-[13px]
+                <div className="flex flex-row justify-between items-center overflow-hidden">
+                  <div className="flex flex-col gap-[4px]">
+                    {" "}
+                    <h1
+                      className="flex flex-row text-sky-500 text-xl items-center gap-[13px]
   "
-                >
-                  {data?.article.title}
-                  <p
-                    className="flex flex-row text-black text-xs items-center gap-[5px]
+                    >
+                      {data?.article.title}
+                      <p
+                        className="flex flex-row text-black text-xs items-center gap-[5px]
 "
-                  >
-                    <button
-                      className={`flex items-center ${
-                        data?.article.favorited ? "text-red-500" : ""
-                      }`}
-                      onClick={() => handleLike(data?.article)}
-                    >
-                      <HeartIcon
-                        className={`size-[16px] ${
-                          data?.article.favorited ? "fill-current" : ""
-                        }`}
-                      />
-                    </button>
+                      >
+                        <button
+                          className={`flex items-center ${
+                            data?.article.favorited ? "text-red-500" : ""
+                          }`}
+                          onClick={() => handleLike(data?.article)}
+                        >
+                          <HeartIcon
+                            className={`size-[16px] ${
+                              data?.article.favorited ? "fill-current" : ""
+                            }`}
+                          />
+                        </button>
 
-                    {data?.article.favoritesCount}
-                  </p>
-                </h1>
-                <div className="flex flex-row gap-[8px] ">
-                  {data?.article.tagList?.map((tag: string, i: number) => (
-                    <span
-                      className="border-[1px] border-black border-solid text-xs px-[5px] py-[2px] text-center rounded-xs"
-                      key={i}
-                    >
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-              </div>
-              <div className="flex flex-row gap-[12px]">
-                <div className="flex flex-col">
-                  {" "}
-                  <h1
-                    className="text-lg
+                        {data?.article.favoritesCount}
+                      </p>
+                    </h1>
+                    <div className="flex flex-row gap-[8px] ">
+                      {data?.article.tagList?.map((tag: string, i: number) => (
+                        <span
+                          className="border-[1px] border-black border-solid text-xs px-[5px] py-[2px] text-center rounded-xs"
+                          key={i}
+                        >
+                          {tag}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-[12px]">
+                    <div className="flex flex-col">
+                      {" "}
+                      <h1
+                        className="text-lg
    text-black capitalize"
-                  >
-                    {data?.article.author?.username}
-                  </h1>
-                  <h2 className="text-xs text-gray-500">
-                    {data?.article.createdAt
-                      ? format(new Date(data?.article.createdAt), "PPP")
-                      : ""}
-                  </h2>
-                </div>
-                <img
-                  src={
-                    data?.article.author?.image
-                      ? data.article.author?.image
-                      : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9tmItyQlUn7ktlHH10GC4cu_-znWFXXW-w&s"
-                  }
-                  alt="avatar"
-                  className="rounded-[50%] block size-[46px]  object-cover
+                      >
+                        {data?.article.author?.username}
+                      </h1>
+                      <h2 className="text-xs text-gray-500">
+                        {data?.article.createdAt
+                          ? format(new Date(data?.article.createdAt), "PPP")
+                          : ""}
+                      </h2>
+                    </div>
+                    <img
+                      src={
+                        data?.article.author?.image
+                          ? data.article.author?.image
+                          : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9tmItyQlUn7ktlHH10GC4cu_-znWFXXW-w&s"
+                      }
+                      alt="avatar"
+                      className="rounded-[50%] block size-[46px]  object-cover
   "
-                />
-              </div>
-            </div>
-            <div
-              className="flex justify-between items-center
+                    />
+                  </div>
+                </div>
+                <div
+                  className="flex justify-between items-center
 "
-            >
-              {" "}
-              <p className="text-xs text-gray-600 mt-[4px]">
-                {data?.article.description}
-              </p>
-              <div
-                className={`flex flex-row gap-[12px] ${
-                  data?.article.author.username !== userInfo.username
-                    ? `hidden`
-                    : ``
-                }`}
-              >
-                <Popconfirm
-                  title=""
-                  description="Are you sure to delete this article?"
-                  onConfirm={confirm}
-                  okText="Yes"
-                  cancelText="No"
-                  placement="right"
                 >
-                  <button className="text-center text-sm text-red-600 px-[17px] py-[6px] border border-red-600 rounded-sm">
-                    Delete
-                  </button>
-                </Popconfirm>
-                <Link
-                  to={`/articles/${data?.article.slug}/edit`}
-                  state={{ article: data?.article }}
-                  className="text-center text-lime-500 text-sm px-[17px] py-[6px] border border-lime-500 rounded-sm"
-                >
-                  Edit
-                </Link>
-              </div>
-            </div>
-
-            <section className="flex flex-wrap overflow-hidden">
-              <Markdown>{data?.article.body || ""}</Markdown>
-            </section>
+                  {" "}
+                  <p className="text-xs text-gray-600 mt-[4px]">
+                    {data?.article.description}
+                  </p>
+                  <div
+                    className={`flex flex-row gap-[12px] ${
+                      data?.article.author.username !== userInfo.username
+                        ? `hidden`
+                        : ``
+                    }`}
+                  >
+                    <Popconfirm
+                      title=""
+                      description="Are you sure to delete this article?"
+                      onConfirm={confirm}
+                      okText="Yes"
+                      cancelText="No"
+                      placement="right"
+                    >
+                      <button className="text-center text-sm text-red-600 px-[17px] py-[6px] border border-red-600 rounded-sm">
+                        Delete
+                      </button>
+                    </Popconfirm>
+                    <Link
+                      to={`/articles/${data?.article.slug}/edit`}
+                      state={{ article: data?.article }}
+                      className="text-center text-lime-500 text-sm px-[17px] py-[6px] border border-lime-500 rounded-sm"
+                    >
+                      Edit
+                    </Link>
+                  </div>
+                </div>
+                <section className="flex flex-wrap overflow-hidden">
+                  <Markdown>{data?.article.body || ""}</Markdown>
+                </section>
+              </>
+            )}
           </div>
         </article>
       )}
--- app/routes/articles.tsx
+++ app/routes/articles.tsx
@@ -23,7 +23,7 @@ export default function Articles() {
 
   const offset = (currentPage - 1) * 5;
 
-  const { data, isLoading, isError } = useGetArticlesQuery(offset);
+  const { data, isLoading, error } = useGetArticlesQuery(offset);
 
   const [likeArticle] = useLikeArticleMutation();
   const [removeLike] = useRemoveLikeMutation();
@@ -39,14 +39,6 @@ export default function Articles() {
       console.error("Like error:", err);
     }
   };
-  if (isError) {
-    return (
-      <div className="text-red-600 flex flex-row justify-center items-center pt-[251px]">
-        <ExclamationCircleIcon className="size-5" />{" "}
-        <p className="text-xl">{isError}</p>
-      </div>
-    );
-  }
 
   return (
     <>
@@ -57,93 +49,102 @@ export default function Articles() {
         </div>
       ) : (
         <div className="pt-[26px]">
-          <ul className="flex flex-col gap-[26px] px-[251px] py-[6px]">
-            {data?.articles.map((article: any) => (
-              <li
-                key={`${article.slug}-${article.createdAt}`}
-                className="bg-white p-[16px] shadow-md"
-              >
-                <div className="flex flex-row justify-between items-center overflow-hidden">
-                  <div className="flex flex-col gap-[4px]">
-                    {" "}
-                    <h1
-                      className="flex flex-row text-[#1890FF] text-xl items-center gap-[13px]
+          {error ? (
+            <div className="text-red-600 flex flex-row justify-center items-center gap-3">
+              <ExclamationCircleIcon className="size-5" />{" "}
+              <p className="text-xl">Oh no! Something went wrong!</p>
+            </div>
+          ) : (
+            <>
+              <ul className="flex flex-col gap-[26px] px-[251px] py-[6px]">
+                {data?.articles.map((article: any) => (
+                  <li
+                    key={`${article.slug}-${article.createdAt}`}
+                    className="bg-white p-[16px] shadow-md"
+                  >
+                    <div className="flex flex-row justify-between items-center overflow-hidden">
+                      <div className="flex flex-col gap-[4px]">
+                        {" "}
+                        <h1
+                          className="flex flex-row text-[#1890FF] text-xl items-center gap-[13px]
 "
-                    >
-                      <Link to={`/articles/${article.slug}`}>
-                        {article.title}
-                      </Link>
-                      <p
-                        className="flex flex-row text-black text-xs items-center gap-[5px]
-"
-                      >
-                        <button
-                          className={`flex items-center ${
-                            article.favorited ? "text-red-500" : ""
-                          }`}
-                          onClick={() => handleLike(article)}
                         >
-                          <HeartIcon
-                            className={`size-[16px] ${
-                              article.favorited ? "fill-current" : ""
-                            }`}
-                          />
-                        </button>
+                          <Link to={`/articles/${article.slug}`}>
+                            {article.title}
+                          </Link>
+                          <p
+                            className="flex flex-row text-black text-xs items-center gap-[5px]
+"
+                          >
+                            <button
+                              className={`flex items-center ${
+                                article.favorited ? "text-red-500" : ""
+                              }`}
+                              onClick={() => handleLike(article)}
+                            >
+                              <HeartIcon
+                                className={`size-[16px] ${
+                                  article.favorited ? "fill-current" : ""
+                                }`}
+                              />
+                            </button>
 
-                        {article.favoritesCount}
-                      </p>
-                    </h1>
-                    <div className="flex flex-row gap-[8px] ">
-                      {article.tagList.map((tag: string, i: number) => (
-                        <span
-                          className="border-[1px] border-black border-solid text-xs px-[5px] py-[2px] text-center rounded-xs"
-                          key={i}
-                        >
-                          {tag}
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                  <div className="flex flex-row gap-[12px]">
-                    <div className="flex flex-col">
-                      {" "}
-                      <h1
-                        className="text-lg
+                            {article.favoritesCount}
+                          </p>
+                        </h1>
+                        <div className="flex flex-row gap-[8px] ">
+                          {article.tagList.map((tag: string, i: number) => (
+                            <span
+                              className="border-[1px] border-black border-solid text-xs px-[5px] py-[2px] text-center rounded-xs"
+                              key={i}
+                            >
+                              {tag}
+                            </span>
+                          ))}
+                        </div>
+                      </div>
+                      <div className="flex flex-row gap-[12px]">
+                        <div className="flex flex-col">
+                          {" "}
+                          <h1
+                            className="text-lg
  text-black capitalize"
-                      >
-                        {article.author.username}
-                      </h1>
-                      <h2
-                        className="text-xs text-gray-500
+                          >
+                            {article.author.username}
+                          </h1>
+                          <h2
+                            className="text-xs text-gray-500
 "
-                      >
-                        {format(new Date(article.createdAt), "PPP")}
-                      </h2>
+                          >
+                            {format(new Date(article.createdAt), "PPP")}
+                          </h2>
+                        </div>
+                        <img
+                          src={
+                            article.author.image
+                              ? article.author.image
+                              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9tmItyQlUn7ktlHH10GC4cu_-znWFXXW-w&s"
+                          }
+                          alt="avatar"
+                          className="rounded-[50%] block size-[46px] object-cover
+"
+                        />
+                      </div>
                     </div>
-                    <img
-                      src={
-                        article.author.image
-                          ? article.author.image
-                          : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl9tmItyQlUn7ktlHH10GC4cu_-znWFXXW-w&s"
-                      }
-                      alt="avatar"
-                      className="rounded-[50%] block size-[46px] object-cover
-"
-                    />
-                  </div>
-                </div>
-                <p className="text-xs text-gray-600 mt-[4px]">
-                  {article.description}
-                </p>
-              </li>
-            ))}
-          </ul>
+                    <p className="text-xs text-gray-600 mt-[4px]">
+                      {article.description}
+                    </p>
+                  </li>
+                ))}
+              </ul>
 
-          <Pagination
-            totalCount={data?.articlesCount}
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-          />
+              <Pagination
+                totalCount={data?.articlesCount}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+              />
+            </>
+          )}
         </div>
       )}
     </>
